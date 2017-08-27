9bcd0c84fd5f57ede8b5d52c6ec2b63c8662f501
Igor <igorputina@gmail.com>
2017-08-27 13:00:16 +0300

    Configured the slider

---
+++ assets/js/photos.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var slider = tns({
+  container: '.slider',
+  items: 1,
+  mode: 'carousel',
+  controls: false,
+  autoplay: false,
+  nav: true,
+  arrowKeys: true,
+  autoplayHoverPause: true,
+  autoplayButton: false,
+  autoHeight: true,
+  lazyload: true,
+  mouseDrag: true
+});
